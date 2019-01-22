08f49ee566583ab230af69075fd505f751d0118e
morninng <mr_morninng_star_dash@yahoo.co.jp>
2019-01-22 16:29:44 +0900

    rename agora-io library elements

--- projects/agora-io/src/lib/agora-io.component.ts
+++ projects/agora-io/src/lib/agora-io.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
-import { AgoraIoService, AgoraStream } from './agora-io.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'lib-agora-io',
@@ -9,80 +8,13 @@ import { AgoraIoService, AgoraStream } from './agora-io.service';
 export class AgoraIoComponent implements OnInit, OnDestroy {
 
 
-  current_stream_id_arr: string[] = [];
-  current_stream_arr: AgoraStream[] = [];
-
-  constructor(
-    private change_ref: ChangeDetectorRef,
-    private agoraIoService: AgoraIoService
-  ) { }
+  constructor() { }
 
   ngOnInit() {
-    this.agoraIoService.get_remote_streams$()
-    .subscribe((new_stream_arr: AgoraStream[] = []) => {
-
-
-
-
-      const added_stream = [];
-      const new_stream_id_arr = [];
-      new_stream_arr.forEach((new_stream: AgoraStream) => {
-        const new_stream_id = new_stream.getId();
-        new_stream_id_arr.push(new_stream_id);
-        if (this.current_stream_id_arr.indexOf(new_stream_id) === -1) {
-          added_stream.push(new_stream);
-        }
-      });
-      if (added_stream.length > 0) {
-        this.add_stream(added_stream);
-      }
-
-      const removed_stream = [];
-
-      this.current_stream_arr.forEach((curr_stream: AgoraStream) => {
-        let is_exist = false;
-        const curr_stream_id = curr_stream.getId();
-        new_stream_arr.forEach((new_stream: AgoraStream) => {
-          const new_stream_id = new_stream.getId();
-          if (new_stream_id === new_stream_id) {
-            is_exist = true;
-          }
-        });
-        if (!is_exist) {
-          removed_stream.push(curr_stream);
-        }
-      });
-      this.remove_stream(removed_stream);
-
-      this.current_stream_arr = new_stream_arr;
-      // const current_stream_id_arr = [];
-      // this.current_stream_arr.forEach((stream: AgoraStream) => {
-      //   current_stream_id_arr.push(stream.getId());
-      // });
-      this.current_stream_id_arr = new_stream_id_arr;
-      this.change_ref.detectChanges();
-    });
   }
 
 
-  add_stream(stream_arr: AgoraStream[]) {
-    setTimeout(() => {
-      stream_arr.forEach((stream: AgoraStream) => {
-        stream.play(stream.getId());
-      });
-      this.change_ref.detectChanges();
-    }, 200);
-  }
 
-  remove_stream(stream_arr: AgoraStream[]) {
-    stream_arr.forEach((stream: AgoraStream) => {
-      stream.stop();
-      stream = null;
-    });
-  }
-
-  ngOnDestroy() {
-    // this.remove_stream(this.current_stream_arr);
-  }
+  ngOnDestroy() {}
 
 }
--- projects/agora-io/src/lib/agora-io.module.ts
+++ projects/agora-io/src/lib/agora-io.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AgoraIoComponent } from './agora-io.component';
 import { PublishStreamComponent } from './publish-stream/publish-stream.component';
+import { PlayStreamComponent } from './play-stream/play-stream.component';
 
 @NgModule({
   imports: [
     CommonModule,
   ],
-  declarations: [AgoraIoComponent, PublishStreamComponent],
-  exports: [AgoraIoComponent, PublishStreamComponent]
+  declarations: [AgoraIoComponent, PublishStreamComponent, PlayStreamComponent],
+  exports: [AgoraIoComponent, PublishStreamComponent, PlayStreamComponent]
 })
 export class AgoraIoModule { }
