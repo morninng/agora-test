a16a9702ef6f676e7a04b37cc1f1766d97b91f0f
morninng <mr_morninng_star_dash@yahoo.co.jp>
2019-02-19 17:14:08 +0900

    nested route

--- projects/sample-app2/src/app/app-routing.module.ts
+++ projects/sample-app2/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'livevideo',
+    loadChildren: './livevideo2/livevideo2.module#Livevideo2Module',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/sample-app2/src/app/livevideo2/livevideo2-routing.module.ts
+++ projects/sample-app2/src/app/livevideo2/livevideo2-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AllContainerComponent } from './all-container/all-container.component';
+import { BeforeEnterComponent } from './before-enter/before-enter.component';
+import { GameComponent} from './game/game.component';
+
+const routes: Routes = [{
+  path: '',
+  component: AllContainerComponent,
+  children: [
+    {
+      path: 'before-enter',
+      component: BeforeEnterComponent,
+    },
+    {
+      path: 'game',
+      component: GameComponent,
+    },
+  ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
