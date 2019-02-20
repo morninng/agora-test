e3fdbaac3aa4a86a61a6fdaa3de457a2351470e3
morninng <mr_morninng_star_dash@yahoo.co.jp>
2019-02-21 00:58:25 +0900

    routing added for sample3

--- projects/sample3/src/app/app-routing.module.ts
+++ projects/sample3/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'livevideo',
+    loadChildren: './livevideo3/livevideo3.module#Livevideo3Module',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
